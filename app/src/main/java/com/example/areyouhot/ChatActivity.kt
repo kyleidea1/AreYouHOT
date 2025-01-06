74e4f36c38e65af690a8fa3196850be53469a569
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2025-01-06 14:31:10 +0900

    ✏️ 바텀 네비게이션 바 생성

---
+++ app/src/main/java/com/example/areyouhot/ChatActivity.kt
@@ -0,0 +1,15 @@
+package com.example.areyouhot
+
+import androidx.compose.foundation.content.MediaType.Companion.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChatActivity() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text("not yet")
+    }
+}
