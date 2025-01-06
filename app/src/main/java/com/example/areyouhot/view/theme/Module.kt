65382a55ebb9fec74ce9ec16f0b7d776fc7bd8b9
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2025-01-06 22:35:41 +0900

    ✏️ Module에 BackButton 추가

--- app/src/main/java/com/example/areyouhot/view/theme/Module.kt
+++ app/src/main/java/com/example/areyouhot/view/theme/Module.kt
@@ -8,13 +8,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -42,6 +46,24 @@ fun Divider(height: Int) {
         .fillMaxWidth()
         .height(height.dp)
         .background(Color.LightGray))
+}
+
+@Composable
+fun BackButton(onclick: () -> Unit) {
+    IconButton(
+        onClick = onclick,
+        modifier = Modifier
+            .fillMaxHeight()
+            .aspectRatio(1f)
+    ) {
+        Icon(
+            imageVector = Icons.Filled.ArrowBack,
+            contentDescription = "뒤로 가기",
+            modifier = Modifier
+                .aspectRatio(1f)
+                .padding(5.dp)
+        )
+    }
 }
 
 @Composable
