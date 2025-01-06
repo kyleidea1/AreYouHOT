7c3ba7edd66f93a1e01fce526a8454a95942c8a8
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2025-01-06 15:05:30 +0900

    ✏️ 불꽃 커서 시도 - 실패했지만 재밌음

--- app/src/main/java/com/example/areyouhot/MainActivity.kt
+++ app/src/main/java/com/example/areyouhot/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.areyouhot.ui.theme.AreYouHOTTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,3 +17,17 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContent {
+//            AreYouHOTTheme {
+//                WithFireAnimation {
+//                    MainScreen()
+//                }
+//            }
+//        }
+//    }
+//}
