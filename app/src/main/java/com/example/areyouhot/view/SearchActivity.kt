5cb87be563395271f1d8b438b9bc6cf954ca08b7
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2025-01-07 15:22:10 +0900

    ✏️ SearchActivity 대결 탭 완성 - LazyColumn 적용

--- app/src/main/java/com/example/areyouhot/view/SearchActivity.kt
+++ app/src/main/java/com/example/areyouhot/view/SearchActivity.kt
@@ -4,18 +4,23 @@ import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -40,15 +45,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.areyouhot.model.User
+import com.example.areyouhot.model.chatData
+import com.example.areyouhot.model.sampleUser
+import com.example.areyouhot.model.sampleUserList
 import com.example.areyouhot.view.theme.BackButton
+import com.example.areyouhot.view.theme.Divider
+import com.example.areyouhot.view.theme.Typography
 
 @Composable
 fun SearchActivity() {
@@ -60,7 +72,6 @@ fun SearchActivity() {
             Row(modifier = Modifier
                 .fillMaxWidth()
                 .aspectRatio(7f)
-                .background(Black)
                 .padding(16.dp)) {
                 BackButton { }
                 SearchBar(
@@ -71,6 +82,40 @@ fun SearchActivity() {
                     }
                 )
             }
+            TabRow(
+                modifier = Modifier.fillMaxWidth(),
+                selectedTabIndex = tabLayoutState.intValue,
+                containerColor = Color.White,
+                contentColor = Black,
+                indicator = { tabPositions ->
+                    TabRowDefaults.Indicator(
+                        modifier = Modifier.tabIndicatorOffset(tabPositions[tabLayoutState.intValue]),
+                        color = Black
+                    )
+                }
+            ) {
+                // 탭 아이템들
+                Tab(
+                    selected = tabLayoutState.intValue == 0,
+                    onClick = { tabLayoutState.intValue = 0 },
+                    selectedContentColor = Black,
+                    unselectedContentColor = Gray,
+                    text = { Text("대결") }
+                )
+                Tab(
+                    selected = tabLayoutState.intValue == 1,
+                    onClick = { tabLayoutState.intValue = 1 },
+                    selectedContentColor = Black,
+                    unselectedContentColor = Gray,
+                    text = { Text("적수") }
+                )
+            }
+
+            // 탭 컨텐츠
+            when (tabLayoutState.intValue) {
+                0 -> EnemyList(enemyList = sampleUserList)
+                1 -> EmptyScreen(1)
+            }
         }
     }
 }
@@ -79,6 +124,65 @@ fun SearchActivity() {
 fun EmptyScreen(tabIndex: Int) {
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text("$tabIndex")
+    }
+}
+
+@Composable
+fun EnemyList(enemyList: List<User>) {
+    Box(Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(
+                count = enemyList.size,
+                key = { index -> enemyList[index].nickname },
+                contentType = { index -> enemyList[index] }
+            ) { index ->
+                EnemyItem(enemyList[index])
+                Divider(1f)
+            }
+        }
+    }
+}
+
+@Composable
+private fun EnemyItem(enemy: User) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(4f)
+            .clickable {}
+            .padding(10.dp)
+    ) {
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxHeight()
+                .aspectRatio(1f)
+        ) {
+            Image(
+                painter = painterResource(id = enemy.profileImage),
+                contentDescription = enemy.nickname + "Profile",
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(8.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(enemy.nickname, style = Typography.bodyLarge)
+            }
+            Spacer(modifier = Modifier.padding(5.dp))
+            Text(enemy.tier.tierName, modifier = Modifier.fillMaxWidth(), style = Typography.bodyMedium)
+        }
     }
 }
 
@@ -181,5 +285,5 @@ fun SearchBar(
 @Preview(showBackground = true)
 @Composable
 fun SearchActivityPreview() {
-    SearchActivity()
+    EnemyItem(sampleUser)
 }
